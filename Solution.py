218071809460c82ec3693d43787cf9a1dfe57977
Karamax <karamax@russia.ru>
2018-09-21 11:17:29 +0500

    Update Solution.py

--- Solution.py
+++ Solution.py
@@ -1,11 +1,30 @@
-string='Привет'
 #ToDo
-#Read from file
+#Read from file:+
 #Sort alphabetically
 #Percentage
 #Cyrillic symbols only
-#Reduction to the general register
-dic={}
-for x in string:
-    dic[x]=string.count(x)
-print(dic)
+#Reduction to the general register:+
+
+def sol():
+    stringl=string.lower()	
+    length=len(stringl)
+    dic={}
+    for x in stringl:
+        dic[x]=stringl.count(x)
+    print(dic)
+
+print("Choose a mode:")
+print("1-Manual input")
+print("2-Read from file")
+mode=input()
+
+if mode=="2":
+    name=input("Input file name: ")
+    file = open(name+'.txt', 'r')
+    string=file.read()
+    sol()
+elif mode=="1":
+    string=(input("Input string: "))
+    sol()
+else:
+    print("Wrong mode") 
