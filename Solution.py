cb17b1a3e40895c1c83919a0ed3c312d1af064d8
Karamax <karamax@russia.ru>
2018-09-21 06:31:37 +0500

    First commit
    A decision and a development plan have been added.

---
+++ Solution.py
@@ -0,0 +1,11 @@
+string='Привет'
+#ToDo
+#Read from file
+#Sort alphabetically
+#Percentage
+#Cyrillic symbols only
+#Reduction to the general register
+dic={}
+for x in string:
+    dic[x]=s.count(x)
+print(dic)
---
+++ ToDo.md
@@ -0,0 +1,6 @@
+# ToDo:
+- Read from file
+- Sort alphabetically
+- Percentage
+- Cyrillic symbols only
+- Reduction to the general register
