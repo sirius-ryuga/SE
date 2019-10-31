597078a65994f3e224821cbc37382f1e26bf6c71
Maria Ivanchenko <draco_aeternitas@icloud.com>
2019-10-31 16:05:30 +0500

    Update Launcher.py
    PEP8 reformat.

--- Launcher.py
+++ Launcher.py
@@ -7,9 +7,9 @@ path=str(os.getcwd())+"\Env"
 if os.path.exists(path):
         os.system(r'start.bat')
 else:
-        filecont='"'+sys.exec_prefix+"\python.exe"+'"'+" -m venv %CD%\Env && %CD%\Env\Scripts"+"\\"+"activate.bat"
-        filecont+=" && python -m pip install --upgrade pip && pip install chardet && @echo All components installed && @pause"
-        file=open('prepare.bat', 'w')
+        filecont = '"'+sys.exec_prefix+"\python.exe"+'"'+" -m venv %CD%\Env && %CD%\Env\Scripts"+"\\"+"activate.bat"
+        filecont += " && python -m pip install --upgrade pip && pip install chardet && @echo All components installed && @pause"
+        file = open('prepare.bat', 'w')
         file.write(filecont)
         file.close()
         os.system(r'prepare.bat')
